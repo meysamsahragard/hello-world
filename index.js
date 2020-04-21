52e239b5d72800bb79c37105e2aaea5cf118b8bf
meysam <meysam.sahragard@outlook.com>
2020-04-21 09:53:55 +0430

    adding cookie input

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 var json2xls = require('json2xls');
 const fs = require('fs');
-const url = 'https://metawave.ir/makejson.php?&panel_amar_tedadhaghighi=0&panel_amar_tedadhoghooghi=0&panel_amar_hajmhaghighi=0&panel_amar_hajmhoghooghi=0&panel_amar_hajmhaghighihoghooghi=0&panel_amar_kharidhaghighi1=0&panel_amar_kharidhaghighi2=&panel_amar_forooshhaghighi1=0&panel_amar_forooshhaghighi2=&panel_amar_kharidhoghooghi1=0&panel_amar_kharidhoghooghi2=&panel_amar_forooshhoghooghi1=0&panel_amar_forooshhoghooghi2=&panel_amar_saf=0&panel_amar_faselehgheimat=0&panel_amar_hajmhaghighibekol=0&panel_amar_ghodrat_haghighi=0&panel_amar_hajmhoghooghibekol=0&panel_amar_hajmhaghighibekol_sell=0&panel_amar_hajmhoghooghibekol_sell=0&panel_amar_hajmkol=1&panel_amar_hajmkol_val=0&panel_amar_khordeforooshi=0&panel_bazarpaie=0&_=1584639647510';
-const options = {
-  url: url,
-  headers: {
-    Cookie: "PHPSESSID=8k6rmotk9en58n8med5fp361l5; _ga=GA1.2.1328222899.1586880202; _gid=GA1.2.119358716.1586880202"
-  }
-};
-Cookie: "PHPSESSID=tuoj8nc1emi64f4g82vivs2u97; _ga=GA1.2.2144405978.1584188776; ssupp.vid=XCqW4tH2V; ssupp.chatid=null; ssupp.visits=3; _gid=GA1.2.1911236001.1584639579"
+
 express()
   .use(cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/',(req, res)=> res.sendFile('views/index.html', {root: __dirname }))
-  .get('/excelapi', (req, res) => {
+  .get('/', (req, res) => res.sendFile('views/index.html', { root: __dirname }))
+  .get('/excelapi/:cookie', (req, res) => {
     try {
+      const url = 'https://metawave.ir/makejson.php?&panel_amar_tedadhaghighi=0&panel_amar_tedadhoghooghi=0&panel_amar_hajmhaghighi=0&panel_amar_hajmhoghooghi=0&panel_amar_hajmhaghighihoghooghi=0&panel_amar_kharidhaghighi1=0&panel_amar_kharidhaghighi2=&panel_amar_forooshhaghighi1=0&panel_amar_forooshhaghighi2=&panel_amar_kharidhoghooghi1=0&panel_amar_kharidhoghooghi2=&panel_amar_forooshhoghooghi1=0&panel_amar_forooshhoghooghi2=&panel_amar_saf=0&panel_amar_faselehgheimat=0&panel_amar_hajmhaghighibekol=0&panel_amar_ghodrat_haghighi=0&panel_amar_hajmhoghooghibekol=0&panel_amar_hajmhaghighibekol_sell=0&panel_amar_hajmhoghooghibekol_sell=0&panel_amar_hajmkol=1&panel_amar_hajmkol_val=0&panel_amar_khordeforooshi=0&panel_bazarpaie=0&_=1584639647510';
+      let cookie = req.params.cookie;
+      const options = {
+        url: url,
+        headers: {
+          Cookie: cookie
+        }
+      };
+      console.log('test  ' + cookie)
+
       request(options, function (error, response, body) {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
